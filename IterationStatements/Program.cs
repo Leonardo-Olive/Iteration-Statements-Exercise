a688627b7ba9f064529ead30b20b80cc84b12029
leonardo oliveira <leonardo_olivesilva@icloud.com>
2023-01-22 12:22:44 -0800

    Exercises Completed

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,33 +1,126 @@
-﻿namespace IterationStatements
+﻿using System.ComponentModel;
+
+namespace IterationStatements
 {
     public class Program
     {
-        //LukeWarm Section: Create methods below
-        //Write a method that will print to the console all numbers 1000 through - 1000
-
-        //Write a method that will print to the console numbers 3 through 999 by 3 each time
-
-        //Write a method to accept two integers as parameterss and check whether they are equal or not
-        
-        //Write a method to check whether a given number is even or odd
-        
-        //Write a method to check whether a given number is positive or negative
-        
-        //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
-        //Parse()
-        //TryParse()
-
-        //Heatin Up Section:
-        //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
-        //Write a method to display the multiplication table(from 1 to 12) of a given integer
 
 
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            Multi();
+            Console.ReadLine();
         }
+
+        //Write a method that will print to the console all numbers 1000 through - 1000
+        public static void PrintThousand()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        //Write a method that will print to the console numbers 3 through 999 by 3 each time
+        public static void Threes()
+        {
+            for (int k = 3; k <= 999; k += 3)
+            {
+                Console.WriteLine(k);
+            }
+        }
+        //Write a method to accept two integers as parameterss and check whether they are equal or not
+        public static bool IsEqual(int a, int b)
+        {
+            var check = (a == b) ? true : false;
+            return check;
+        }
+        //Write a method to check whether a given number is even or odd
+        public static void Odd()
+        {
+            Console.WriteLine("Write a number");
+            int num = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Your number is even!");
+            }
+            else
+                Console.WriteLine("Your number is odd!");
+        }
+
+        // Write a method to check whether a given number is positive or negative
+
+        public static void Check()
+        {
+            Console.WriteLine("Please enter a number");
+            var number = int.Parse(Console.ReadLine());
+
+
+            if (number >= 0)
+            {
+                Console.WriteLine("Your number is positive");
+            }
+            else
+                Console.WriteLine("Your number is negative");
+            Console.ReadLine();
+
+            //Write a method to read the age of a candidate and determine whether they can vote.
+
+        }
+        public static void Vote()
+        {
+            Console.WriteLine("Please enter your age:");
+
+            var age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("You can vote!");
+
+            }
+            else
+                Console.WriteLine("I love your excitement, but you are not old enough.");
+            Console.ReadLine();
+
+        }
+
+        //Write a method to check if an integer(from the user) is in the range -10 to 10
+
+        public static void Range()
+        {
+
+
+            Console.WriteLine("Let`s guess the number range:");
+
+
+            var input = int.Parse(Console.ReadLine());
+
+            if (input > 10)
+            {
+                Console.WriteLine("Your number is not in the range.");
+            }
+            else
+                Console.WriteLine($"{input} is in the range.");
+
+        }
+
+
+        //Write a method to display the multiplication table(from 1 to 12) of a given integer
+
+        public static void Multi()
+        {
+            Console.WriteLine("Now, lets think of a number to multiply from 1 to 12.");
+            var multNumb = int.Parse(Console.ReadLine());
+
+            
+
+            Console.WriteLine($"Here are the results: {multNumb*1},{multNumb*2}, {multNumb*3}, {multNumb * 4}, {multNumb * 5}, {multNumb * 6}, {multNumb * 7}, {multNumb * 8}, {multNumb * 9},{multNumb * 10}, {multNumb * 11}, {multNumb * 12}");
+        }
+
+
+
+
     }
 }
